ac9eed832e1fc1ff55a143260425c1eccdbff1c9
Maxime MARTIN <maxime.martin@st.com>
2023-03-27 13:34:13 +0200

    fix: use less indentations

--- libs/pipeline_lib.py
+++ libs/pipeline_lib.py
@@ -3,7 +3,7 @@ import time
 import os
 from pathlib import Path
 from logging import Logger
-import threading
+from threading import Thread
 
 # MODELS
 from models.config import Config
@@ -41,7 +41,7 @@ class PipeLine:
             self.logger.info("Pipeline started...")
             self.running = True
 
-            self.thread = threading.Thread(target=self._process, args=(callback,))
+            self.thread = Thread(target=self._process, args=(callback,))
             self.thread.start()
 
     def _process(self, callback):
@@ -50,51 +50,58 @@ class PipeLine:
                 path=self.config.path.input, sort_by_modification_date=True
             )
 
-            if not nbr_klarfs == 0:
-                for klarf_path in klarf_paths:
-                    klarf = os.path.basename(klarf_path)
-                    klarf_name, klarf_extension = os.path.splitext(klarf)
+            if nbr_klarfs == 0:
+                # Add a sleep here to avoid CPU hogging
+                time.sleep(0.1)
+                continue
 
-                    try:
-                        if file.check_file_size(klarf_path):
-                            output_path = (
-                                Path(self.config.path.output)
-                                / f"{klarf_name}_clustered{klarf_extension}"
-                            )
+            for klarf_path in klarf_paths:
+                klarf = os.path.basename(klarf_path)
+                klarf_name, klarf_extension = os.path.splitext(klarf)
 
-                            results = self.clustering.apply(
-                                klarf_path=klarf_path,
-                                output_path=output_path,
-                            )
-
-                            if not len(results) == 0 and os.path.exists(klarf_path):
-                                os.remove(klarf_path)
-
-                                if callback is not None:
-                                    callback(results)
-                            else:
-                                self.logger.error(
-                                    msg=f"Unable to remove {klarf=}",
-                                )
-
-                    except Exception as ex:
-
-                        if os.path.exists(klarf_path):
-                            file.move(
-                                src=klarf_path,
-                                dest=os.path.join(self.config.path.error, klarf),
-                            )
-
-                        message_error = mailing.send_mail_error(
-                            klarf=klarf,
-                            error_path=self.config.path.error,
-                            config=self.config.mailing,
+                try:
+                    if not file.check_file_size(klarf_path):
+                        self.logger.warning(
+                            f"Timeout exceeded to process {klarf_path=}"
                         )
 
-                        self.logger.critical(
-                            msg=message_error,
-                            exc_info=ex,
+                        continue
+
+                    output_path = (
+                        Path(self.config.path.output)
+                        / f"{klarf_name}_clustered{klarf_extension}"
+                    )
+
+                    results = self.clustering.apply(
+                        klarf_path=klarf_path,
+                        output_path=output_path,
+                    )
+
+                    if not len(results) == 0 and os.path.exists(klarf_path):
+                        os.remove(klarf_path)
+
+                        if callback is not None:
+                            callback(results)
+                    else:
+                        self.logger.error(
+                            msg=f"Unable to remove {klarf=}",
                         )
 
-                    # Add a sleep here to avoid CPU hogging
-                    time.sleep(0.1)
+                except Exception as ex:
+
+                    if os.path.exists(klarf_path):
+                        file.move(
+                            src=klarf_path,
+                            dest=os.path.join(self.config.path.error, klarf),
+                        )
+
+                    message_error = mailing.send_mail_error(
+                        klarf=klarf,
+                        error_path=self.config.path.error,
+                        config=self.config.mailing,
+                    )
+
+                    self.logger.critical(
+                        msg=message_error,
+                        exc_info=ex,
+                    )
