f17eeafd7ef7afc0430c539f5a61dff77955d36f
Maxime MARTIN <maxime.martin@st.com>
2023-06-27 14:55:14 +0200

    feat: use watchdog to monitor new files + upgrade wafermap-clustering

--- wafermap_clustering_pipeline/core.py
+++ wafermap_clustering_pipeline/core.py
@@ -1,16 +1,44 @@
 # MODULES
 import platform
+import time
+import os
+from multiprocessing import Pool
 from pathlib import Path
+from logging import Logger
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from wafermap_clustering_pipeline.libs.process_lib import Process
 
 # CONFIGS
 from .configs.config import Config
 from .configs.logging import setup_logger
 
 
-if __name__ == "__main__":
-    # LIBS
-    from .libs.pipeline_lib import PipeLine
+class FileHandler(FileSystemEventHandler):
+    def __init__(
+        self,
+        pool,
+        process: Process,
+        logger: Logger,
+    ):
+        super().__init__()
+        self._pool = pool
+        self._process = process
+        self._logger = logger
 
+    def on_created(self, event):
+        # Check if the event is for a file
+        if event.is_directory:
+            return
+
+        self._logger.info(f"New file {(file_path := event.src_path)} detected")
+
+        # Trigger a task to process the new file
+        self._pool.apply_async(self._process.process_klarf, args=(file_path,))
+
+
+if __name__ == "__main__":
     CONFIGS = Config(
         platform=platform.system().lower(),
         conf_path=Path(__file__).parent / "configs" / "config.json",
@@ -21,5 +49,44 @@ if __name__ == "__main__":
         directory=Path(CONFIGS.directories.logs),
     )
 
-    pipeline = PipeLine(config=CONFIGS, logger=LOGGER)
-    pipeline.start()
+    LOGGER.info(
+        f"Creation of process pool (max_workers={CONFIGS.multi_processing.max_workers})"
+    )
+
+    # Create a process pool to manage the worker processes
+    pool = Pool(processes=CONFIGS.multi_processing.max_workers)
+
+    # Create the process instance
+    process = Process(config=CONFIGS)
+
+    LOGGER.info(
+        f"Creation of a file watcher to monitor new files from {CONFIGS.directories.input})"
+    )
+
+    # Create a file watcher to monitor the folder for new files
+    event_handler = FileHandler(pool, process, LOGGER)
+    observer = Observer()
+    observer.schedule(event_handler, path=CONFIGS.directories.input, recursive=False)
+    observer.start()
+
+    files = [
+        os.path.join(CONFIGS.directories.input, f)
+        for f in os.listdir(CONFIGS.directories.input)
+        if os.path.isfile(os.path.join(CONFIGS.directories.input, f))
+    ]
+
+    if (num_files := len(files)) > 0:
+        LOGGER.info(
+            f"Start the process of {num_files} file already in {CONFIGS.directories.input})"
+        )
+
+        # Submit a task to process each file
+        for file_path in files:
+            pool.apply_async(process.process_klarf, args=(file_path,))
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
--- wafermap_clustering_pipeline/libs/process_lib.py
+++ wafermap_clustering_pipeline/libs/process_lib.py
@@ -1,11 +1,6 @@
 # MODULES
-import math
-import time
 import os
-import multiprocessing as mp
-from typing import Tuple
 from pathlib import Path
-from logging import Logger
 
 # WAFERMAP-CLUSTERING
 from wafermap_clustering.wafermap_clustering import Clustering
@@ -20,106 +15,46 @@ from ..utils import mailing, file
 
 
 class Process:
-    def __init__(self, config: Config, logger: Logger) -> None:
+    def __init__(self, config: Config) -> None:
         self.config = config
-        self.logger = logger
-        self.running = False
         self.clustering = Clustering(config=self.config)
 
-    def process_klarfs(
+    def process_klarf(
         self,
+        klarf_path: Path,
+        logger_name: str = "process_klarf",
     ):
-        while self.running:
-            klarf_paths, nbr_klarfs = file.get_files(
-                path=self.config.directories.input,
-                sort_by_modification_date=True,
-            )
-
-            if nbr_klarfs == 0:
-                # Add a sleep here to avoid CPU hogging
-                time.sleep(self.config.cpu_hog_tempo)
-                continue
-
-            tic = time.time()
-
-            multi_processing = (
-                self.config.multi_processing.use_multi_processing
-                and nbr_klarfs >= self.config.multi_processing.num_files
-            )
-            if multi_processing:
-                max_workers = min(
-                    mp.cpu_count(), self.config.multi_processing.max_workers
-                )
-                with mp.Pool(processes=max_workers) as pool:
-                    chunksize = max(
-                        1,
-                        int(math.ceil(len(klarf_paths) / max_workers)),
-                    )
-                    chunks = [
-                        (klarf_paths[i : i + chunksize])
-                        for i in range(0, len(klarf_paths), chunksize)
-                    ]
-                    results = pool.starmap(
-                        self.process_chunks,
-                        iterable=[
-                            (chunk, self.config.directories.output) for chunk in chunks
-                        ],
-                    )
-                    results = [item for sublist in results for item in sublist]
-            else:
-                for klarf_path in klarf_paths:
-                    results = self.process_klarf(
-                        klarf_path=klarf_path,
-                        output_dir=self.config.directories.output,
-                    )
-
-            self.logger.info(
-                f"Batch of {nbr_klarfs} klarf(s) executed in {time.time() - tic}s [{multi_processing=}]"
-            )
-
-    def process_chunks(self, chunk: Tuple[Path], output_dir: str):
-        return [
-            self.process_klarf(klarf_path=item, output_dir=output_dir) for item in chunk
-        ]
-
-    def process_klarf(self, klarf_path: Path, output_dir: Path):
         # get the current process
         process_id = os.getpid()
         logger = setup_logger(
-            name="process_klarf",
+            name=logger_name,
             directory=Path(self.config.directories.logs),
         )
 
         klarf = os.path.basename(klarf_path)
 
         try:
-            if file.check_file_size(klarf_path, timeout=self.config.time_out):
-                results = self.clustering.apply(
-                    klarf_path=klarf_path,
-                    output_directory=output_dir,
-                    klarf_format=self.config.klarf_returned,
-                    clustering_mode=self.config.clustering_algo,
-                )
-
-                [
-                    logger.info(
-                        msg=f"{process_id=} processed ({repr(clustering)}) with {self.config.clustering_algo} in {clustering.performance.clustering_timestamp}s [defects={len(clustering.clustered_defects)}, clusters={clustering.clusters}] [klarf ({self.config.klarf_returned}) generated in {clustering.performance.output_timestamp}s]"
-                    )
-                    for clustering in results
-                ]
-
-                if len(results) == 0 or not os.path.exists(klarf_path):
-                    return logger.error(msg=f"Unable to remove {klarf=}")
-
-                os.remove(klarf_path)
-
-                return results
-
-        except TimeoutError as ex:
-            logger.warning(
-                msg=f"Timeout exceeded while cheking size of {klarf_path=}",
-                exc_info=ex,
+            results = self.clustering.apply(
+                klarf_path=klarf_path,
+                output_directory=self.config.directories.output,
+                klarf_format=self.config.klarf_returned,
+                clustering_mode=self.config.clustering_algo,
             )
+
+            [
+                logger.info(
+                    msg=f"{process_id=} processed ({repr(clustering)}) with {self.config.clustering_algo} in {clustering.performance.clustering_timestamp}s [defects={len(clustering.clustered_defects)}, clusters={clustering.clusters}] [klarf ({self.config.klarf_returned}) generated in {clustering.performance.output_timestamp}s]"
+                )
+                for clustering in results
+            ]
+
+            if len(results) == 0 or not os.path.exists(klarf_path):
+                return logger.error(msg=f"Unable to remove {klarf=}")
+
+            os.remove(klarf_path)
+
+            return results
+
         except Exception as ex:
             if os.path.exists(klarf_path):
                 file.move(src=klarf_path, dest=self.config.directories.error / klarf)
